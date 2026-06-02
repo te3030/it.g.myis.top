3361ec03235830348030d45de0020c004c74b64d
te3030 <te3030@users.noreply.github.com>
2026-06-02 06:53:31 +0000

    deploy: te3030/github_sync@dd76e8f2875164589d9c7126e2daa7c98765c59b

---
+++ assets/___vite-browser-external_commonjs-proxy-abc06e95.js
@@ -0,0 +1,1 @@
+import{ax as e}from"./index-62297d5b.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
---
+++ assets/json-minify-08859207.js
@@ -0,0 +1,5 @@
+import{_ as a}from"./FormatTransformer.vue_vue_type_script_setup_true_lang-16341b33.js";import{l as r}from"./index-231c3e11.js";import{w as i}from"./defaults-4d6daddf.js";import{d as s,o as l,c as p}from"./index-62297d5b.js";import"./TextareaCopyable-f3208634.js";import"./Copy-593e5723.js";import"./Scrollbar-a573c257.js";const u=`{
+	"hello": [
+		"world"
+	]
+}`,N=s({__name:"json-minify",setup(m){const t=o=>i(()=>JSON.stringify(r.parse(o),null,0),""),e=[{validator:o=>o===""||r.parse(o),message:"Provided JSON is not valid."}];return(o,f)=>{const n=a;return l(),p(n,{"input-label":"Your raw JSON","input-default":u,"input-placeholder":"Paste your raw JSON here...","output-label":"Minified version of your JSON","output-language":"json","input-validation-rules":e,transformer:t})}}});export{N as default};
